06f833e3bf4f5ca1e10f3f7ab4581b7f9b43b81a
Victor Buldakov <victor@buldakov.org>
2019-11-09 13:36:48 +0000

    init

---
+++ Assets/Scripts/AStar/BreadCrumb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class BreadCrumb : IComparable<BreadCrumb>
+{
+	public readonly Point position;
+	public BreadCrumb prev;
+	public BreadCrumb next;
+	public int cost = int.MaxValue;
+	public bool onClosedList;
+	public bool onOpenList;
+
+	public BreadCrumb (Point position)
+	{
+		this.position = position;
+	}
+
+	//Overrides default Equals so we check on position instead of object memory location
+	public override bool Equals (object obj)
+	{
+		return (obj is BreadCrumb) && ((BreadCrumb)obj).position.X == position.X && ((BreadCrumb)obj).position.Y == position.Y;
+	}
+
+	//Faster Equals for if we know something is a BreadCrumb
+	public bool Equals (BreadCrumb breadcrumb)
+	{
+		return breadcrumb.position.X == position.X && breadcrumb.position.Y == position.Y;
+	}
+
+	public override int GetHashCode ()
+	{
+		return position.GetHashCode ();
+	}
+
+	public int CompareTo (BreadCrumb other)
+	{
+		return cost.CompareTo (other.cost);
+	}
+}
+
---
+++ Assets/Scripts/NodeConnection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NodeConnection
+{
+	public Node Parent;
+	public Node Node;
+	public bool Valid;
+
+	public NodeConnection (Node parent, Node node, bool valid)
+	{
+		Valid = valid;
+		Node = node;
+		Parent = parent;
+
+		if (Node != null && Node.BadNode)
+			Valid = false;
+		if (Parent != null && Parent.BadNode)
+			Valid = false;
+	}
+}
