f33a6925dae20880d96ef197ea4f21f8474b6da5
Victor Buldakov <victor@buldakov.org>
2019-11-10 01:22:19 +0000

    remove old code and add new grid implementation

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,312 +1,70 @@
 ﻿using UnityEngine;
 
-public enum Direction
-{
-    Right,
-    Left,
-    Top,
-    Bottom,
-    BottomLeft,
-    BottomRight,
-    TopLeft,
-    TopRight,
-}
-
 public class Grid : MonoBehaviour
 {
-    public LayerMask CollisionMask;
+    public LayerMask unwalkableMask;
+    public Vector2 gridSize;
+    public float nodeRadius;
+    Node[,] grid;
 
-    [HideInInspector]
-    public int Width;
-    [HideInInspector]
-    public int Height;
+    float nodeDiameter;
+    int gridSizeX;
+    int gridSizeY;
 
-    public Node[,] Nodes;
-
-    public int Left { get { return 0; } }
-    public int Right { get { return Width; } }
-    public int Bottom { get { return 0; } }
-    public int Top { get { return Height; } }
-
-    public const float UnitSize = 1f;
-
-    void Awake()
+    private void Start()
     {
-        Width = ((int)transform.localScale.x) * 2 + 2;
-        Height = ((int)transform.localScale.y) * 2 + 2;
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridSize.y / nodeDiameter);
+        CreateGrid();
+    }
 
-        Nodes = new Node[Width, Height];
-
-        //Initialize the grid nodes - 1 grid unit between each node
-        //We render the grid in a diamond pattern
-        for (int x = 0; x < Width / 2; x++)
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 bottomLeft = transform.position - Vector3.right * gridSize.x / 2f - Vector3.up * gridSize.y / 2f;
+        for (int i = 0; i < gridSizeX; i++)
         {
-            for (int y = 0; y < Height; y++)
+            for (int j = 0; j < gridSizeY; j++)
             {
-                float ptx = x;
-                float pty = -(y / 2) + (UnitSize / 2f);
-                int offsetx = 0;
-
-                if (y % 2 == 0)
-                {
-                    ptx = x + (UnitSize / 2f);
-                    offsetx = 1;
-                }
-                else
-                {
-                    pty = -(y / 2);
-                }
-
-                Vector2 pos = new Vector2(ptx, pty);
-                Node node = new Node(x * 2 + offsetx, y, pos, this);
-                Nodes[x * 2 + offsetx, y] = node;
-            }
-        }
-
-        //Create connections between each node
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                if (Nodes[x, y] == null) 
-                {
-                    continue;
-                }
-                Nodes[x, y].InitializeConnections(this);
-            }
-        }
-
-        //Pass 1, we removed the bad nodes, based on valid connections
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                if (Nodes[x, y] == null)
-                {
-                    continue;
-                }
-
-                Nodes[x, y].CheckConnectionsPass1(this);
-            }
-        }
-
-        //Pass 2, remove bad connections based on bad nodes
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                if (Nodes[x, y] == null)
-                    continue;
-
-                Nodes[x, y].CheckConnectionsPass2();
+                Vector3 worldPoint = bottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.up * (j * nodeDiameter + nodeRadius);
+                bool walkable = !Physics2D.OverlapBox(worldPoint, Vector2.one * nodeDiameter, 0, unwalkableMask);
+                grid[i, j] = new Node(walkable, worldPoint);
             }
         }
     }
 
-
-    public Point WorldToGrid(Vector2 worldPosition)
+    public Node GetNodeFromWorldPoint(Vector2 pos)
     {
-        Vector2 gridPosition = new Vector2((worldPosition.x * 2f), -(worldPosition.y * 2f) + 1);
+        float percentX = (pos.x + gridSize.x / 2f) / gridSize.x;
+        float percentY = (pos.y + gridSize.y / 2f) / gridSize.y;
 
-        //adjust to our nearest integer
-        float rx = gridPosition.x % 1;
-        if (rx < 0.5f)
-            gridPosition.x -= rx;
-        else
-            gridPosition.x += (1 - rx);
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
 
-        float ry = gridPosition.y % 1;
-        if (ry < 0.5f)
-            gridPosition.y -= ry;
-        else
-            gridPosition.y += (1 - ry);
-
-        int x = (int)gridPosition.x;
-        int y = (int)gridPosition.y;
-
-        if (x < 0 || y < 0 || x > Width || y > Height)
-            return null;
-
-        Node node = Nodes[x, y];
-        //We calculated a spot between nodes'
-        //Find nearest neighbor
-        if ((node == null) || (x % 2 == 0 && y % 2 == 0) || (y % 2 == 1 && x % 2 == 1))
-        {
-            float mag = 100;
-
-
-            if (x < Width && !Nodes[x + 1, y].BadNode)
-            {
-                float mag1 = (Nodes[x + 1, y].Position - worldPosition).magnitude;
-                if (mag1 < mag)
-                {
-                    mag = mag1;
-                    node = Nodes[x + 1, y];
-                }
-            }
-            if (y < Height - 1 && !Nodes[x, y + 1].BadNode)
-            {
-                float mag1 = (Nodes[x, y + 1].Position - worldPosition).magnitude;
-                if (mag1 < mag)
-                {
-                    mag = mag1;
-                    node = Nodes[x, y + 1];
-                }
-            }
-            if (x > 0 && !Nodes[x - 1, y].BadNode)
-            {
-                float mag1 = (Nodes[x - 1, y].Position - worldPosition).magnitude;
-                if (mag1 < mag)
-                {
-                    mag = mag1;
-                    node = Nodes[x - 1, y];
-                }
-            }
-            if (y > 0 && !Nodes[x, y - 1].BadNode)
-            {
-                float mag1 = (Nodes[x, y - 1].Position - worldPosition).magnitude;
-                if (mag1 < mag)
-                {
-                    mag = mag1;
-                    node = Nodes[x, y - 1 + 1];
-                }
-            }
-        }
-        if (node == null) {
-            return null;
-        }
-        return new Point(node.X, node.Y);
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
     }
 
-    public Vector2 GridToWorld(Point gridPosition)
+    private void OnDrawGizmos()
     {
-        Vector2 world = new Vector2(gridPosition.X / 2f, -(gridPosition.Y / 2f - 0.5f));
-
-        return world;
-    }
-
-    public bool ConnectionIsValid(Point point1, Point point2)
-    {
-        //comparing same point, return false
-        if (point1.X == point2.X && point1.Y == point2.Y)
-            return false;
-
-        if (Nodes[point1.X, point1.Y] == null)
-            return false;
-
-        //determine direction from point1 to point2
-        Direction direction = Direction.Bottom;
-
-        if (point1.X == point2.X)
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, gridSize.y, 1));
+        if (grid != null)
         {
-            if (point1.Y < point2.Y)
-                direction = Direction.Bottom;
-            else if (point1.Y > point2.Y)
-                direction = Direction.Top;
-        }
-        else if (point1.Y == point2.Y)
-        {
-            if (point1.X < point2.X)
-                direction = Direction.Right;
-            else if (point1.X > point2.X)
-                direction = Direction.Left;
-        }
-        else if (point1.X < point2.X)
-        {
-            if (point1.Y > point2.Y)
-                direction = Direction.TopRight;
-            else if (point1.Y < point2.Y)
-                direction = Direction.BottomRight;
-        }
-        else if (point1.X > point2.X)
-        {
-            if (point1.Y > point2.Y)
-                direction = Direction.TopLeft;
-            else if (point1.Y < point2.Y)
-                direction = Direction.BottomLeft;
-        }
-
-        //check connection
-        switch (direction)
-        {
-            case Direction.Bottom:
-                if (Nodes[point1.X, point1.Y].Bottom != null)
-                    return Nodes[point1.X, point1.Y].Bottom.Valid;
-                else
-                    return false;
-
-            case Direction.Top:
-                if (Nodes[point1.X, point1.Y].Top != null)
-                    return Nodes[point1.X, point1.Y].Top.Valid;
-                else
-                    return false;
-
-            case Direction.Right:
-                if (Nodes[point1.X, point1.Y].Right != null)
-                    return Nodes[point1.X, point1.Y].Right.Valid;
-                else
-                    return false;
-
-            case Direction.Left:
-                if (Nodes[point1.X, point1.Y].Left != null)
-                    return Nodes[point1.X, point1.Y].Left.Valid;
-                else
-                    return false;
-
-            case Direction.BottomLeft:
-                if (Nodes[point1.X, point1.Y].BottomLeft != null)
-                    return Nodes[point1.X, point1.Y].BottomLeft.Valid;
-                else
-                    return false;
-
-            case Direction.BottomRight:
-                if (Nodes[point1.X, point1.Y].BottomRight != null)
-                    return Nodes[point1.X, point1.Y].BottomRight.Valid;
-                else
-                    return false;
-
-            case Direction.TopLeft:
-                if (Nodes[point1.X, point1.Y].TopLeft != null)
-                    return Nodes[point1.X, point1.Y].TopLeft.Valid;
-                else
-                    return false;
-
-            case Direction.TopRight:
-                if (Nodes[point1.X, point1.Y].TopRight != null)
-                    return Nodes[point1.X, point1.Y].TopRight.Valid;
-                else
-                    return false;
-
-            default:
-                return false;
-        }
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (Nodes == null)
-        {
-            return;
-        }
-        for (int i = 0; i < Width; i++)
-        {
-            for (int j = 0; j < Height; j++)
+            foreach (Node node in grid)
             {
-                var node = Nodes[i, j];
-                if (node == null)
+                if (node.walkable)
                 {
-                    continue;
+                    Gizmos.color = Color.white;
                 }
-                if (node.BadNode)
+                else
                 {
                     Gizmos.color = Color.red;
                 }
-                else
-                {
-                    Gizmos.color = Color.yellow;
-                }
-                Gizmos.DrawSphere(node.Position, 0.1f);
+                Gizmos.DrawCube(node.position, Vector3.one * (nodeDiameter - 0.1f));
             }
         }
     }
+
 }
