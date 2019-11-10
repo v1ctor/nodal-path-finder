00510309ac32c65594ea82ee33405fef99cb1607
Victor Buldakov <victor@buldakov.org>
2019-11-10 16:07:48 +0000

    make units to follow path smoothly

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Line
+{
+    const float verticalLineGradient = 1e5f;
+    float gradient;
+    float intercept;
+    float perpendicular;
+    Vector2 pointOnLine1;
+    Vector2 pointOnLine2;
+
+    bool approachSide;
+
+    public Line(Vector2 pointOnLine, Vector2 pointPerpendicular) {
+        float dx = pointOnLine.x - pointPerpendicular.x;
+        float dy = pointOnLine.x - pointPerpendicular.y;
+        
+        if (dx == 0) {
+            perpendicular = verticalLineGradient;
+        } else {
+            perpendicular = dy / dx;
+        }
+        if (perpendicular == 0) {
+            gradient = verticalLineGradient;
+        } else {
+            gradient = -1 / perpendicular;
+        }
+
+        intercept = pointOnLine.y - gradient * pointOnLine.x;
+        pointOnLine1 = pointOnLine;
+        pointOnLine2 = pointOnLine + new Vector2(1, gradient);
+
+        approachSide = false;
+        approachSide = GetSide(pointPerpendicular);
+    }
+
+    bool GetSide(Vector2 p) {
+        return (p.x - pointOnLine1.x) * (pointOnLine2.y - pointOnLine1.y) > (p.y - pointOnLine1.y) * (pointOnLine2.x - pointOnLine1.x);
+    }
+
+    public bool HasCrossedLine(Vector2 p) {
+        return GetSide(p) != approachSide;
+    }
+
+    public void DrawWithGizmos(float length) {
+        Vector3 lineDir = new Vector3(1, gradient).normalized;
+        Vector3 lineCenter = new Vector3(pointOnLine1.x, pointOnLine1.y);
+        Gizmos.DrawLine(lineCenter - lineDir * length / 2f, lineCenter + lineDir * length / 2f);
+    }
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Path 
+{
+    public readonly Vector2[] lookPoints;
+    public readonly Line[] turnBoundaries;
+    public readonly int finishLineIndex;
+
+    public Path(Vector2[] lookPoints, Vector2 startPoint, float turnDst)
+    {
+        this.lookPoints = lookPoints;
+        this.turnBoundaries = new Line[lookPoints.Length];
+        this.finishLineIndex = lookPoints.Length - 1;
+
+        Vector2 prevPoint = startPoint;
+        for (int i = 0; i < lookPoints.Length; i++) {
+            Vector2 currentPoint = lookPoints[i];
+            Vector2 dirToCurPoint = (currentPoint - prevPoint).normalized;
+            Vector2 turnBoundaryPoint = (i == finishLineIndex) ? currentPoint : currentPoint - dirToCurPoint * turnDst;
+            turnBoundaries[i] = new Line(turnBoundaryPoint, prevPoint - dirToCurPoint * turnDst);
+            prevPoint = turnBoundaryPoint;
+        }
+    }
+
+    public void DrawWithGizmos() {
+        Gizmos.color = Color.black;
+        foreach (Vector2 p in lookPoints) {
+            Gizmos.DrawCube(p, Vector3.one);
+        }
+
+        Gizmos.color = Color.white;
+        foreach(Line l in turnBoundaries) {
+            l.DrawWithGizmos(10);
+        }
+    }
+}
